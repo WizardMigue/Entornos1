bf6512609451992f4453d86e2439862222c91aa4
DAM1ALU15 <DAM1ALU15@B01-PC15.IESGUARA.LOCAL>
2022-04-19 10:15:17 +0200

    Cambio minimo

--- patron_builder/src/patron_builder/Main.java
+++ patron_builder/src/patron_builder/Main.java
@@ -3,11 +3,8 @@ package patron_builder;
 public class Main {
 
 	public static void main(String[] args) {
-		Usuario usu = new UsuarioBuilder()
-				.email("hola@esViernes.com")
-				.nombre("Juan", "Gonzalez")
-				.direccion("Calle Perena, 25")
-				.build();
+		Usuario usu = new UsuarioBuilder().email("hola@esViernes.com").nombre("Juan", "Gonzalez")
+				.direccion("Calle Perena, 25").build();
 		System.out.println(usu);
 
 	}
